b60253e44b01719889121f4a5417f4409c36773f
Ezra <eschwartz929@gmail.com>
2024-03-08 04:51:58 -0500

    adding images and fixing bugs

--- dice-game/src/Game.js
+++ dice-game/src/Game.js
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react"
+import './Game.css'
 
 function Game() {
-    const [dice, setDice] = useState([{value: 1, locked: false}, {value: 1, locked: false}, {value: 1, locked: false}, {value: 1, locked: false}, {value: 1, locked: false}])
-    const [turn, setTurn] = useState(0)
+    const defaultGame = [
+        {value: 1, locked: false}, 
+        {value: 2, locked: false}, 
+        {value: 3, locked: false}, 
+        {value: 4, locked: false}, 
+        {value: 5, locked: false}
+    ]
+    const [dice, setDice] = useState(defaultGame)
+    const [rollsRemaining, setRollsRemaining] = useState(3)
     const [winner, setWinner] = useState(false)
     const [completed, setCompleted] = useState(false)
 
@@ -14,7 +22,7 @@ function Game() {
             }
         }
         setDice(arr)
-        setTurn(turn+1)
+        setRollsRemaining(rollsRemaining - 1)
         checkIfWinner()
     }
 
@@ -23,7 +31,7 @@ function Game() {
 
         for (var i = 1; i < dice.length; i++) {
             if (dice[i].value !== val) {
-                if (turn === 3) {
+                if (rollsRemaining - 1 === 0) {
                     setCompleted(true)
                 }
                 return
@@ -34,7 +42,7 @@ function Game() {
         setCompleted(true)
     }
 
-    function lockDie(index) {
+    function lockDice(index) {
         var arr = [...dice]
         arr[index].locked = !arr[index].locked
         setDice(arr)
@@ -42,37 +50,41 @@ function Game() {
 
     function restartGame() {
         setCompleted(false)
-        setTurn(1)
-        roll()
+        setDice(defaultGame)
+        setRollsRemaining(3)
     }
 
     return (
         <>
             <h3>Game</h3>
-            <h4>Turn: {turn}</h4>
+            
 
-            <div>
+            <div className="dice-wrapper">
                 {dice.map((item, index) => 
-                    <Die key={index} value={item.value} index={index} locked={item.locked} setLocked={() => lockDie(index)}/>
+                    <Dice key={index} value={item.value} index={index} locked={item.locked} setLocked={() => lockDice(index)}/>
                 )}
             </div>
 
             {!completed && <button onClick={roll}>Roll Dice</button>}
-            {winner && <span>Congrats! You have Won!</span>}
-            {!winner && turn === 3 && <span>Not this time</span>}
-            {completed && <button onClick={restartGame}>New Game</button>}
+            {completed && <button onClick={restartGame}>New Game</button>
+            }
+            <h4>{rollsRemaining} rolls left</h4>
+
+            {winner && <span>Winner!</span>}
+            {!winner && completed && <span>Try Again</span>}
         </>
 
     )
 }
 
-function Die({value, index, locked, setLocked}) {
+function Dice({value, index, locked, setLocked}) {
 
     return (
-        <>
-            <span>{value}</span>
+        <div className="dice">
+            <img src={"dice" + value + ".png"} alt={value} width="40" height="40"/>
+            <br/>
             <button onClick={() => setLocked(index)}>{locked ? 'Unlock' : 'Lock'}</button>
-        </>
+        </div>
     )
 }
 
